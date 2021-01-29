b56618753a743153d901f7caab49f2d28d72c497
pabloschonwiesner <pabloschonwiesner@gmail.com>
2021-01-29 00:31:39 -0300

    saque el modulo de body-parser que no lo estoy usando

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const PORT = 8080
 
-const app = express()
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-
 app.use(require('./routes'))
-
-
 
 const server = app.listen(PORT, () => console.log(`Servidor funcionando en el puerto ${PORT}`))
 server.on('error', (err) => console.log(`Error en el servidor: ${err}`))
